a72252dede535f1b702ef0eac4f989f2409a765b
최주희 <elfinnani@gmail.com>
2026-09-15 10:22:14 +0900

    run.py: DRY_RUN 기본값 버그 수정 (미설정 시 dry-run으로 떨어지던 문제)

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ import os, sys
 from graph import run
 
 if __name__ == "__main__":
-    if "--dry-run" in sys.argv:
-        os.environ["DRY_RUN"] = "1"
+    os.environ["DRY_RUN"] = "1" if "--dry-run" in sys.argv else "0"
     out = run()
     for line in out["log"]:
         print(line)
